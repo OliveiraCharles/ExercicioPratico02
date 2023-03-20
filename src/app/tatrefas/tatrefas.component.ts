3b298e6c5a012a504226b4be389a0a4a736be4a7
Charles Oliveira <105311867+OliveiraCharles@users.noreply.github.com>
2023-03-20 14:06:53 -0300

    added task counter

--- src/app/tatrefas/tatrefas.component.ts
+++ src/app/tatrefas/tatrefas.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./tatrefas.component.css']
 })
 export class TatrefasComponent {
+  contagemTarefas: number = 0
   tarefas = [
     { descricao: 'Tarefa 01', concluida: false },
     { descricao: 'Tarefa 02', concluida: false },
@@ -13,4 +14,32 @@ export class TatrefasComponent {
     { descricao: 'Tarefa 04', concluida: false },
     { descricao: 'Tarefa 05', concluida: false }
   ];
+
+  concluirTodas() {
+    for (let tarefa of this.tarefas) {
+      tarefa.concluida = true
+    }
+    this.atualizar()
+  }
+  refazerTodas() {
+    for (let tarefa of this.tarefas) {
+      tarefa.concluida = false
+    }
+    this.atualizar()
+  }
+
+  atualizar() {
+    this.contagemTarefas = 0
+    for (let tarefa of this.tarefas) {
+      if (tarefa.concluida == true) {
+        this.contagemTarefas = this.contagemTarefas + 1
+      }
+    }
+    return this.contagemTarefas
+  }
+
+  concluir(tarefa: any) {
+    tarefa.concluida = !tarefa.concluida;
+    this.atualizar()
+  }
 }
